1bbb415abc4e2fce4db488c087a6c472cc7a0bdf
79ceca7e-1710-4439-8f8b-c115f316e9e9 <github-enterprise+79ceca7e-1710-4439-8f8b-c115f316e9e9@flatironschool.com>
2020-02-11 15:22:20 -0800

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,19 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  return nil if name_hash == {}
+  min_value = nil
+  min_key = nil
+  name_hash.each do |k, v|
+    if min_value == nil && min_key == nil
+      min_value = v
+      min_key = k
+      next
+    end
+    if v < min_value
+      min_value = v
+      min_key = k
+    end
+  end
+  min_key
 end
